25e2c4e9fb4c042ceeaee62ef2312b1c6141e4b1
samarjaffal <samarjaffalh@gmail.com>
2020-12-05 09:35:42 -0400

    added logic for remove team

--- helpers/team-helper.js
+++ helpers/team-helper.js
@@ -58,10 +58,52 @@ module.exports = {
                 }
             }
 
+            await crudHelper.removeSet('users', userId, 'users', {
+                teams: ObjectID(teamId),
+            });
+
             await crudHelper.removeSet(collection, teamId, 'teams', {
                 members: ObjectID(userId),
             });
 
+            return true;
+        } catch (error) {
+            console.log(error);
+            throw new Error(error);
+        }
+    },
+
+    deleteTeam: async (teamId) => {
+        let team, members, projectIds;
+
+        try {
+            team = await mongoDB.get(collection, ObjectID(teamId));
+
+            if (!team) {
+                return false;
+            }
+
+            if ('projects' in team && team.projects.length > 0) {
+                projectIds = [...team.projects];
+                if (projectIds.length > 0) {
+                    let query = { _id: { $in: projectIds } };
+                    await mongoDB.deleteMany('projects', query);
+                }
+            }
+
+            if ('members' in team && team.members.length > 0) {
+                members = [...team.members];
+
+                if (members.length > 0) {
+                    let query = { _id: { $in: members } };
+                    let operator = {
+                        $pull: { teams: ObjectID(teamId) },
+                    };
+                    await mongoDB.updateMany('users', query, operator);
+                }
+            }
+
+            await mongoDB.delete(collection, teamId);
             return true;
         } catch (error) {
             console.log(error);
