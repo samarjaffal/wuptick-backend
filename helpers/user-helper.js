7faa3368a4b152dcbb73dc028758aa2e3353c071
samarjaffal <samarjaffalh@gmail.com>
2020-12-27 18:30:58 -0400

    added toggleFavTask mutation and function

--- helpers/user-helper.js
+++ helpers/user-helper.js
@@ -1,6 +1,8 @@
 const MongoLib = require('../lib/db/mongo');
 const { ObjectID } = require('mongodb');
 const mongoDB = new MongoLib();
+const crudHelper = require('./crud-helper');
+const collection = 'users';
 
 const getUserInfo = async (userId) => {
     try {
@@ -11,4 +13,35 @@ const getUserInfo = async (userId) => {
     }
 };
 
-module.exports = { getUserInfo };
+const addFavoriteTask = async (userId, taskId) => {
+    let user;
+    const operator = {
+        format: { favorite_tasks: ObjectID(taskId) },
+        key: 'favorite_tasks',
+        set: 'favorite_tasks',
+    };
+    await crudHelper.addSet(collection, userId, operator, 'user');
+    return taskId;
+};
+
+const removeFavoriteTask = async (userId, taskId) => {
+    let user;
+    const operator = { favorite_tasks: ObjectID(taskId) };
+    await crudHelper.removeSet(collection, userId, 'user', operator);
+    return taskId;
+};
+
+const toggleFavTask = async (state, userId, taskId) => {
+    try {
+        if (state) {
+            return await addFavoriteTask(userId, taskId);
+        } else {
+            return await removeFavoriteTask(userId, taskId);
+        }
+    } catch (error) {
+        console.log(error);
+        throw new Error(error);
+    }
+};
+
+module.exports = { getUserInfo, toggleFavTask };
