b6ca9d9fe48c504e11fa1a5f2d067877d589ca25
samarjaffal <samarjaffalh@gmail.com>
2021-01-21 15:17:06 -0400

    added uploadEditorImage function (without authentication verification)

--- routes/uploadEditorImage.js
+++ routes/uploadEditorImage.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const MongoLib = require('../lib/db/mongo');
 const { ObjectID } = require('mongodb');
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const { cloudinary } = require('../utils/cloudinary');
 
 const uploadEditorImage = (app) => {
     const router = express.Router();
     app.use('/', router);
     router.use(express.json({ limit: '50mb' }));
     router.use(express.urlencoded({ limit: '50mb', extended: true }));
+
     router.post('/upload_editor_image', async (req, res) => {
         try {
             const fileStr = req.body.data;
-            console.log(fileStr, 'fileStr');
+            /* console.log(fileStr, 'fileStr'); */
 
-            //return response
-            return res.send({
-                ok: true,
+            const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
+                upload_preset: 'dev-tests',
             });
-        } catch (error) {}
+
+            console.log(uploadedResponse, 'uploadedResponse');
+            
+            return res.send({
+                success: 1,
+                file: {
+                    url: uploadedResponse.secure_url,
+                    width: uploadedResponse.width,
+                    height: uploadedResponse.height,
+                },
+            });
+        } catch (error) {
+            res.status(500).json({ success: 0, file: {} });
+        }
     });
 };
 
