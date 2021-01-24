c61e685b8a9ce98de9939459068fb0c1a72219af
samarjaffal <samarjaffalh@gmail.com>
2021-01-24 16:37:11 -0400

    improved createComment mutation

--- helpers/comment.js
+++ helpers/comment.js
@@ -5,6 +5,13 @@ const crudHelper = require('./crud-helper');
 const collection = 'comments';
 const { ObjectID } = require('mongodb');
 
+const defaults = {
+    page: 1,
+    count: 1,
+    comments: [],
+};
+const maximumComments = 6;
+
 module.exports = {
     getCommentsForTask: async (taskId) => {
         let comments;
@@ -18,5 +25,59 @@ module.exports = {
         }
 
         return comments || [];
+    },
+
+    createComment: async (input) => {
+        let comment, taskId, topicId, commentBelowMax;
+        let query;
+        let newComment;
+
+        if ('task' in input) taskId = ObjectID(input.task._id);
+        if ('topic' in input) topicId = ObjectID(input.topic._id);
+
+        if ('comments' in input) {
+            input.comments._id = ObjectID();
+            input.comments.owner = ObjectID(input.comments.owner._id);
+            if (!'commentJson' in input.comments) {
+                input.comments.commentJson = '';
+            }
+        }
+
+        if (taskId !== null) {
+            query = { task: ObjectID(taskId), count: { $lt: maximumComments } };
+        } else if (topicId !== null) {
+            query = {
+                topic: ObjectID(topicId),
+                count: { $lt: maximumComments },
+            };
+        }
+
+        commentBelowMax = await mongoDB.getAll(collection, query);
+
+        if (commentBelowMax.length > 0) {
+            [newComment] = [commentBelowMax][0];
+
+            /* newComment.comments = [...newComment.comments]; */
+            newComment.comments.unshift({ ...input.comments });
+
+            newComment.count = newComment.comments.length;
+
+            await mongoDB.update(collection, newComment._id, newComment);
+            comment = newComment;
+        } else {
+            delete query.count;
+            let countPages = await mongoDB.getAll(collection, query, true);
+            let pages = countPages + 1;
+
+            newComment = {
+                comments: [{ ...input.comments }],
+                task: taskId,
+                topic: topicId,
+                page: pages,
+            };
+            comment = await crudHelper.create(collection, newComment, defaults);
+        }
+
+        return comment;
     },
 };
