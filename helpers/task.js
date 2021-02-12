56c04fe35426f0896f5db063877cf2312352ee5e
samarjaffal <samarjaffalh@gmail.com>
2021-02-11 21:51:48 -0400

    added findMentions, getMentionIds to get mentionIds

--- helpers/task.js
+++ helpers/task.js
@@ -37,11 +37,45 @@ module.exports = {
             if ('tag' in inputData) inputData.tag = ObjectID(inputData.tag._id);
             if ('assigned' in inputData)
                 inputData.assigned = ObjectID(input.assigned._id);
+            let mentionIds = module.exports.findMentions(inputData.description);
+            console.log(mentionIds, 'mentions');
             task = await crudHelper.edit(collection, taskId, inputData, 'task');
         } catch (error) {
             console.error(error);
         }
         return task;
+    },
+
+    findMentions: (html) => {
+        console.log(html, 'description');
+        const myregexp = /<span[^>]+?class="mention-item".*?>([\s\S]*?)<\/span>/g;
+        let match = myregexp.exec(html);
+        let mentions = [];
+        while (match != null) {
+            mentions.push(match[0]);
+            match = myregexp.exec(html);
+        }
+
+        return mentions.length > 0
+            ? module.exports.getMentionIds(mentions)
+            : [];
+    },
+
+    getMentionIds: (mentions) => {
+        let mentionIds = [];
+        let match;
+
+        mentions.forEach((mentionStr) => {
+            const regex = new RegExp(
+                '[\\s\\r\\t\\n]*([a-z0-9\\-_]+)[\\s\\r\\t\\n]*=[\\s\\r\\t\\n]*([\'"])((?:\\\\\\2|(?!\\2).)*)\\2',
+                'ig'
+            );
+            while ((match = regex.exec(mentionStr))) {
+                if (match[1] == 'data-value') mentionIds.push(match[3]);
+            }
+        });
+
+        return mentionIds;
     },
 
     getTasks: async (moduleId) => {
